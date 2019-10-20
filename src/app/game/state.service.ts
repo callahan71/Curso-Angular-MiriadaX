c6eaec901635e089e2c0411d7e65129889c1898f
Jaime Quirant <jimmie.codes@gmail.com>
2019-10-20 20:02:01 +0200

    Primer commit tictactoe

---
+++ src/app/game/state.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+//import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { BehaviorSubject } from 'rxjs';
+
+export interface State {
+  turn: string,
+  values: string[][]
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StateService {
+
+  private _state$: BehaviorSubject<State>;
+
+  constructor() { 
+    this._state$ = new BehaviorSubject({
+      turn: 'PLAYERX',
+      values: [
+        ['-','-','-'],
+        ['-','-','-'],
+        ['-','-','-']
+      ]
+    });
+  }
+
+  get state$(): BehaviorSubject<State> {
+    return this._state$;
+  }
+
+  get state(): State {
+    return this._state$.getValue();
+  }
+
+  set state(state: State) {
+    this._state$.next(state);
+  }
+
+  updateValue(row, col) {
+    if(this.state.values[row][col] === '-') {
+      let newValue = this.state.turn === 'PLAYERX' ? 'X' : '0';
+      let newTurn = this.state.turn === 'PLAYERX' ? 'PLAYER0' : 'PLAYERX';
+      this.state.values[row][col] = newValue;
+      this.state.turn = newTurn;
+      this.state = this.state;
+    }
+  }
+
+  reset() {
+    this.state = {
+      turn: 'PLAYERX',
+      values: [
+        ['-','-','-'],
+        ['-','-','-'],
+        ['-','-','-']
+      ]
+    }
+  }
+}
